249956bd626fc093017d91231b1fb806d58e2d61
JDMTias <matiasiturbide@gmail.com>
2020-04-24 15:36:57 -0600

    closure question answered, task 2

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,11 @@ myFunction();
 
 // Explanation: 
 
+//  When myFunction is created a new scope is created, a functional scope. Then the function or/and variables created within that scope have the ability to reach outward for context, never inward, and no matter how deep you nest.  Thats why internal is able to reach out and get its information. 
+
+
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
